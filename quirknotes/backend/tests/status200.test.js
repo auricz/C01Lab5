eba306081135d037194ed274635908078bfe9f42
auricz <auric.zhou@gmail.com>
2024-02-14 19:08:53 -0500

    copied test code into file

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -1,22 +1,44 @@
-const SERVER_URL = "http://localhost:4000";
+test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
 
-test("/postNote - Post a note", async () => {
-  const title = "NoteTitleTest";
-  const content = "NoteTitleContent";
+test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
 
-  const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      title: title,
-      content: content,
-    }),
-  });
+test("/deleteNote - Delete a note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
 
-  const postNoteBody = await postNoteRes.json();
+test("/patchNote - Patch with content and title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
 
-  expect(postNoteRes.status).toBe(200);
-  expect(postNoteBody.response).toBe("Note added succesfully.");
+test("/patchNote - Patch with just title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/patchNote - Patch with just content", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/deleteAllNotes - Delete one note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/deleteAllNotes - Delete three notes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
+  // Code here
+  expect(false).toBe(true);
 });
